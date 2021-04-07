6d13d7c17b44c795ef62429459fd0d0d825ad405
bar-schtalman <barschtalman@gmail.com>
2021-04-07 17:36:07 +0300

    fixes

--- Board.cpp
+++ Board.cpp
@@ -5,59 +5,63 @@
 #include <map>
 using namespace std;
 
+
 namespace ariel{
 
     void Board::post(unsigned int row ,unsigned int col ,Direction d,const string &msg){
         if ( d == Direction::Vertical){
-            max_row = max(max_row , row + unsigned(msg.length()));
-        }
-        else {
-          max_col = max(max_col, col + unsigned(msg.length()));
-        }
-  
-    
-        for (unsigned int i = 0; i < msg.length(); i ++){
-            board[row][col] = msg.at(i);
-            if (d == Direction::Vertical){
+            max_row = max( row + unsigned(msg.length()),max_row);
+            for(unsigned int i = 0; i < msg.length(); i ++){
+                this->board[row][col] = msg.at(i);
                 row++;
             }
-            else{
-                col ++; 
+        }
+        else {
+             max_col = max( col + unsigned(msg.length()),max_col);
+             for(unsigned int i = 0; i < msg.length(); i ++){
+                this->board[row][col] = msg.at(i);
+                col++;
             }
         }
-        }
+    }
     
 
   string Board::read(unsigned int row, unsigned int col, Direction d, unsigned int char_num){
         string ans;
-        for (int i = 0; i < char_num; i++){
-            if( board[row].find(col)== board[row].end() || board.find(row) == board.end()){
+        for(unsigned int i = 0 ; i < char_num; i++){
+            if(board.find(row) == board.end()){
+                ans += "_";
+            }
+            else if(board[row].find(col) == board[row].end()){
                 ans += "_";
             }
             else{
-                ans += board[row][col];
+                ans += this->board[row][col];
             }
-            if (d == Direction::Horizontal){
-               col++;
+
+            if(d == Direction::Vertical){
+                row ++;
             }
             else{
-                row++;
-            } 
+                col++;
+            }
+
         }
-        
+
         return ans;
     }
 
     void Board::show(){
-    for (unsigned int i = min_row; i < max_row; i++){
-        for(unsigned int j = min_col; j < max_col; j++){
-            cout << board[i][j];
+        for (unsigned int i = min_row; i < max_row; i ++){
+            for(unsigned int j = min_col; j < max_col; j ++){
+                cout << this->board[i][j];
+            }
+            cout << "\n"; 
         }
-        cout << "\n"; 
     }
-}
 
 }
+    
 
 
 
